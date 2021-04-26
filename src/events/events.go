e866022867e867d8f7c4cb76c5bd577cc1fad506
Zytekaron <zytekaron@gmail.com>
2021-04-26 12:08:25 -0700

    remove "e", add "version"

--- src/events/events.go
+++ src/events/events.go
@@ -10,8 +10,9 @@ type Command func(*discordgo.Session, *discordgo.Message, []string)
 var cmds = map[string]Command{}
 
 func init() {
-	register(commands.EvalCommand, "e", "ev", "eval")
+	register(commands.EvalCommand, "ev", "eval")
 	register(commands.PingCommand, "rsping", "rsonline")
+	register(commands.VersionCommand, "rsv", "rsversion")
 }
 
 func register(run Command, uses ...string) {
